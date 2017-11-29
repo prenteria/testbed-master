e5623690e3709783c7013397dbc8ce77fd9c6679
Paulina R <prenteria3@miners.utep.edu>
2017-11-29 11:51:39 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect, url_for, abort, session
+from flask_bootstrap import Bootstrap
+import os
+app = Flask(__name__)
+
+# Secret key to use a session
+app.config['SECRET_KEY'] = 'F34TF$($e34D';
+
+Bootstrap(app)
+
+@app.route("/")
+def index():
+	return render_template('login.html')
+
+@app.route('/home')
+def home():
+	return render_template('index.html')
+
+
+@app.route('/servers')
+def server():
+    return render_template('server.html')
+
+@app.route('/vms')
+def virtualmachine():
+    return render_template('virtualmachines.html')
+
+@app.route('/workshopunits')
+def workshopunits():
+    return render_template('workshopunits.html')
+
+@app.route('/workshopgroups')
+def workshopgroups():
+    return render_template('workshopgroups.html')
+
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
+
+@app.route('/userprofiles')
+def userprofiles():
+    return render_template('userprofiles.html')
+
+# Sessions allows you to store information specific to a user from one request to the next
+# To use a session you need a secret key
+
+# @app.route() is by default limited to GET requests
+# Allowed HTTP methods of an action can be specified using the methods keyword arg.
+@app.route('/login', methods=['POST'])
+def login():
+    session['email'] = request.form['email']
+    session['password'] = request.form['password']
+    return redirect(url_for('message'))
+
+@app.route('/message')
+def message():
+    if not 'email' in session:
+        return abort(403)
+    return render_template('message.html', email=session['email'], 
+                                           password=session['password'])
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return index()
+
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
